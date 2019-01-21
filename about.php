3ce2a8742a64104349ba75b83290b2e99f1671dd
NA <NA>
2019-01-21 21:43:21 +0530

    change in about page

--- about.php
+++ about.php
@@ -1,4 +1,4 @@
-<!--
+﻿<!--
 author: W3layouts
 author URL: http://w3layouts.com
 License: Creative Commons Attribution 3.0 Unported
@@ -522,3 +522,4 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <!-- //main slider-banner --> 
 </body>
 </html>
+Thsi is changed
